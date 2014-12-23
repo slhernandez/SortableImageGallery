f6cd97741a98285e17bd229a73d9d64207ff480a
Steve Hernandez <slhernandez@gmail.com>
2014-12-23 15:18:28 -0600

    Fleshed out the gulpfile.js with watch, build, site, css, scripts tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -17,5 +17,39 @@ var paths = {
 };
 
 gulp.task('css', function() {
+  return gulp.src(paths.sass)
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest(paths.build + 'css'))
+    .pipe(notify({ message: 'css task complete' }));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src(paths.scripts)
+    .pipe(jshint('.jshint'))
+    .pipe(jshint.reporter('.default'))
+    .pipe(gulp.dest('.build/scripts'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+gulp.task('site', function() {
+  return gulp.src(paths.site)
+    .pipe(gulp.dest(paths.build));
+});
+
+gulp.task('clean', function(cb) {
+  del(paths.build, cb)
+});
+
+gulp.task('watch', function() {
+
+  // Watch scss files
+  gulp.watch(paths.sass, ['styles']);
+
+  // Watch .js files
+  gulp.watch(paths.scripts, ['scripts']);
 
 });
+
+gulp.task('build', ['site', 'css', 'scripts']);
+gulp.task('default', ['build', 'watch']);
